7777a39700248f54bd21c24ff18a242f9d45ec3c
Ridhwankiki <ridhwanr0607@gmail.com>
2024-04-17 10:21:49 +0700

    Make Emoji Row Component

---
+++ EmojiSearch/Utility/RandomColor.swift
@@ -0,0 +1,17 @@
+//
+//  RandomColor.swift
+//  EmojiSearch
+//
+//  Created by MacBook Pro on 17/04/24.
+//
+
+import SwiftUI
+
+struct RandomColor {
+    static func bgView() -> Color {
+        return Color(red: Double.random(in: 0...1),
+                     green: Double.random(in: 0...1),
+                     blue: Double.random(in: 0...1)
+        )
+    }
+}
